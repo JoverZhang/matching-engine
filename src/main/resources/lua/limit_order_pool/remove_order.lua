33acf47698f57c57e5a882416648054ec75e7bac
joverzhang <joverzh@gmail.com>
2021-02-26 13:58:21 +0800

    feat: update lua's `remove_order`

--- src/main/resources/lua/limit_order_pool/remove_order.lua
+++ src/main/resources/lua/limit_order_pool/remove_order.lua
@@ -14,23 +14,22 @@ local function remove_order()
     local mapName = poolName .. '::map'
 
     -- 从 `map` 中删除 `挂单`, 如果 `挂单` 已经不存在, 则直接返回
+    local remainAmount = redis.call('HGET', mapName, order.id)
     local deleted = redis.call('HDEL', mapName, order.id)
     if deleted ~= 1 then
-        return 0
+        return nil
     end
 
     -- 从 `队列` 中移除 `挂单`
     redis.call('LREM', queueName, 1, order.id)
 
-    -- 当 `队列` 在移除挂单后仍存在 `挂单`, 则直接返回
+    -- 从 `挂单池` 中移除空的 `队列`
     local queueLen = redis.call('LLEN', queueName)
-    if queueLen ~= 0 then
-        return 1
+    if queueLen == 0 then
+        redis.call('ZREM', poolName, order.price)
     end
 
-    -- 从 `挂单池` 中移除空的 `队列`
-    redis.call('ZREM', poolName, order.price)
-    return 2
+    return order.id .. ',' .. order.price .. ',' .. remainAmount
 end
 
 return remove_order()
